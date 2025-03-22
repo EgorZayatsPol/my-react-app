874d48201f014e9495efb3fc185e1416188789e8
EgorZayatsPol <egorzayatss@gmail.com>
2025-03-23 00:28:59 +0100

    Initial commit

---
+++ my-react/src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import Counter from "./components/counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
+import Postlist from "./components/Postlist";
+
+function App() {
+    const [posts, setPosts] = useState([
+        {id:1, title:'JavaScript', body : 'Description'},
+        {id:2, title:'JavaScript 2', body : 'Description'},
+        {id:3, title:'JavaScript 3', body : 'Description'}
+    ])
+
+  return (
+    <div className="App">
+        <Postlist posts={posts} title={"1 Posts List"}/>
+    </div>
+  );
+}
+
+export default App;
