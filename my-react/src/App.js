71a536c1ab83727c671d00a558e11eafd2ab8a2f
EgorZayatsPol <egorzayatss@gmail.com>
2025-03-24 01:51:46 +0100

    Add new directory and files

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import Counter from "./components/counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import Postlist from "./components/Postlist";
+import Mybutton from "./components/UI/button/Mybutton";
+import Myinput from "./components/UI/input/Myinput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -12,10 +14,29 @@ function App() {
         {id:3, title:'JavaScript 3', body : 'Description'}
     ])
 
+    const [title, setTitle] = useState('')
+    const bodyInputRef = useRef();
+    const addNewPost = (e) =>{
+     e.preventDefault()
+        console.log(title)
+    }
   return (
     <div className="App">
+        <form>
+            <Myinput
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                type="text"
+                placeholder="name of post"
+            />
+            <Myinput
+                ref={bodyInputRef}
+                type="text"
+                placeholder="description of post"
+            />
+            <Mybutton onClick={addNewPost} disabled>add post</Mybutton>
+        </form>
         <Postlist posts={posts} title={"1 Posts List"}/>
-
     </div>
   );
 }
